f155dae340ee4ed515c67605499ffaa4b1d6aeef
Mohamed Ali Bannour <med.ali.bennour@gmail.com>
2019-03-21 16:41:45 +0100

    changed the OddOccurrencesInArray solution.

--- Lesson2/src/main/scala/arrays/OddOccurrencesInArray.scala
+++ Lesson2/src/main/scala/arrays/OddOccurrencesInArray.scala
@@ -2,25 +2,25 @@ package arrays
 
 
 // Correctness 100%
-// Performance 25 %
+// Performance 100 %
 
-//Task score 66 %
+//Task score 100 %
 object OddOccurrencesInArray extends App {
-
   def solution(a: Array[Int]): Int = {
-    def xxx(l: List[Int]): Int = {
-      l match {
-        case Nil => 0
-        case x :: xs =>
-          if (isCountElementsOdd(x, xs))
-            xxx(xs.filterNot(_ == x))
-          else x
-      }
+    // write your code in Scala 2.12
+  
+      if(a.length == 1) a.head else {
+      count(a.sorted.toList, 1, a.head)
     }
-    xxx(a.toList)
   }
 
-  private def isCountElementsOdd(x: Int, xs: List[Int]) = {
-    xs.count(_ == x) % 2 != 0
-  }
+  def count(list: List[Int], acc: Int, value : Int): Int = list match {
+    case Nil => 0
+    case x :: Nil => if(x == value && (acc + 1) % 2 != 0) x else x
+    case x :: xs => if(x == xs.head) count(xs, acc+ 1, x) else {
+      if(acc % 2 == 0) count(xs, 1, x) else x
+    }
+    
+}
+
 }
